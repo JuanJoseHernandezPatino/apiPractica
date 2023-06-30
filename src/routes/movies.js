75401e1e241e2c301fb5d29c200f08dc66c5d896
Juan Jose Hernandez Patiño <128816341+JuanJoseHernandezPatino@users.noreply.github.com>
2023-06-30 16:22:19 -0500

    Actualización aprendizaje de apis
    Actualización aprendizaje de apis

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,11 +1,26 @@
 let express = require("express");
 let rutas = express.Router()
 let movies = require("../movies.json")
-console.log(movies)
+//console.log(movies)
 
 rutas.get("/", (req, res) => {
     res.send(movies)
 })
+
+rutas.post("/", (req, res) => {
+    console.log(req)
+    const { nombre, clasificacion, director, anio } = req.body
+    if (nombre, clasificacion, director, anio) {
+        const id = movies.length + 1;
+        const newMovie = { id, ...req.body };
+        movies.push(newMovie);
+        res.json(movies)
+    } else {
+        res.send({ "error": "error al ingresar los datos, faltan" })
+    }
+})
+
+
 
 
 module.exports = rutas;
